3abfca7eab855d6e405843a4d4c7f48ca582bd5d
kumarc101203 <kumarchandrashekarnaik@gmail.com>
2026-03-25 16:29:17 +0530

    Routes checked

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,110 @@
+const userModel = require("../models/user.model")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+/**
+ * @name registerUserController
+ * @description register new user, expects username, email, password in body
+ * @access public 
+ */
+async function registerUserController(req, res) {
+
+    const {username, email, password} = req.body
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ 
+            message: "Please Provide Username, email & password" 
+        })
+    }
+
+    const isUserAlreadyExisted = await userModel.findOne({
+        $or: [{email}, {username}]
+    })
+
+    if(isUserAlreadyExisted){
+        return res.status(409).json({
+            message: "User Already Existed with this Email or Username"
+        })
+    }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        username, 
+        email, 
+        password: hash
+    })
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username},
+        process.env.JWT_SECRET,
+        {expiresIn: "1d" }
+    )
+
+res.cookie("token", token)
+
+
+res.status(201).json({
+        message: "User Registered Successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        },
+        token
+    })
+
+}
+
+/**
+ * @name loginUserController
+ * @description login user, expects email, password in body
+ * @access public
+ */
+async  function loginUserController(req, res) {
+    const {email, password} = req.body
+
+    if(!email || !password){
+        return res.status(400).json({
+            message: "Please Provide email and password"
+        })
+    }
+
+    const user = await userModel.findOne({email}).select("+password")
+
+    if(!user){
+        return res.status(404).json({
+            message: "Invalid Credentials"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid){
+        return res.status(400).json({
+            message: "Invalid Credentials"
+        })
+    }
+
+    const token = jwt.sign(
+        { id: user._id, username: user.username},
+        process.env.JWT_SECRET,
+        {expiresIn: "1d" }
+    )
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "User Logged In Successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+module.exports = {
+    registerUserController,
+    loginUserController
+}
