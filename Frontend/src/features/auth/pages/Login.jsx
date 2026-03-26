dad7c9dd235bbeb285b8b3795860e2e64ab0a662
kumarc101203 <kumarchandrashekarnaik@gmail.com>
2026-03-26 13:29:43 +0530

    Added frontend setup and updated backend

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <main>
+      <div className= "form-container" >
+        <h1>
+          Login
+        </h1>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type ="email" id ="email" name ='email' placeholder ='Enter email address' />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type ="password" id ="password" name ='password' placeholder ='Enter password' />
+          </div>
+          <button className='button primary-button'>Login</button>
+        </form>
+      </div>
+    </main>
+  )
+}
+
+export default login
